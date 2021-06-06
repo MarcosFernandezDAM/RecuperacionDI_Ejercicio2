6cb2bea98bd037bd5ae3abea3913431b91b37761
MarcosFernandezDAM <MarcosFernandezDAM@users.noreply.github.com>
2021-06-06 13:00:28 +0200

    version 1

---
+++ Ejercicio2/Aula.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Aula
+    {
+        private int[,] tablaNotas = new int[4, 12];
+
+        public int longitudTabla(int i)
+        {
+            if (i == 0 || i == 1)
+            {
+                return tablaNotas.GetLength(i);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int this[int i, int j]
+        {
+            set
+            {
+                tablaNotas[i, j] = value;
+            }
+            get
+            {
+                return tablaNotas[i, j];
+            }
+        }
+
+        public string[] alumnos = { "Marcos", "Juanjo", "Jan", "Kieran", "Stefan", "Josema", "Mario", "Jorge", "Lucas", "Yanick", "Luis", "Joao" };
+        public string this[int i]
+        {
+            set
+            {
+                alumnos[i] = value;
+            }
+            get
+            {
+                return alumnos[i];
+            }
+        }
+
+        public enum asignaturas
+        {
+            Programación,
+            Bases,
+            Sistemas,
+            Redes
+        }
+
+        public void rellenarTabla()
+        {
+            Random random = new Random();
+            for (int i = 0; i < this.tablaNotas.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.tablaNotas.GetLength(1); j++)
+                {
+                    this.tablaNotas[i, j] = calcularNota(random);
+                }
+            }
+        }
+
+        static int calcularNota(Random random)
+        {
+            int prob = random.Next(1, 101);
+
+            switch (prob)
+            {
+                case int n when n <= 5:
+                    return 0;
+
+                case int n when n <= 10:
+                    return 1;
+
+                case int n when n <= 15:
+                    return 2;
+
+                case int n when n <= 25:
+                    return 3;
+
+                case int n when n <= 40:
+                    return 4;
+
+                case int n when n <= 55:
+                    return 5;
+
+                case int n when n <= 70:
+                    return 6;
+
+                case int n when n <= 80:
+                    return 7;
+
+                case int n when n <= 90:
+                    return 8;
+
+                case int n when n <= 95:
+                    return 9;
+
+                case int n when n <= 100:
+                    return 10;
+
+                default:
+                    return 0;
+            }
+        }
+
+        public double calcularMediaTotal()
+        {
+            double suma = 0;
+            double media;
+            for (int i = 0; i < this.tablaNotas.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.tablaNotas.GetLength(1); j++)
+                {
+                    suma += this.tablaNotas[i, j];
+                }
+            }
+
+            media = suma / (this.tablaNotas.Length);
+            return media;
+        }
+
+        public double calcularMediaAlumno(int numAlumno)
+        {
+            double suma = 0;
+            double media;
+            for (int i = 0; i < this.tablaNotas.GetLength(0); i++)
+            {
+                suma += this.tablaNotas[i, numAlumno - 1];
+            }
+
+            media = suma / this.tablaNotas.GetLength(0);
+            return media;
+        }
+
+        public double calcularMediaAsignatura(int numAsignatura)
+        {
+            double suma = 0;
+            double media;
+            for (int i = 0; i < this.tablaNotas.GetLength(1); i++)
+            {
+                suma += this.tablaNotas[numAsignatura - 1, i];
+            }
+
+            media = suma / this.tablaNotas.GetLength(1);
+            return media;
+        }
+
+        public int[] verNotasAlumno(int numAlumno)
+        {
+            int[] notasAlumno = new int[this.tablaNotas.GetLength(0)];
+            for (int i = 0; i < this.tablaNotas.GetLength(0); i++)
+            {
+                notasAlumno[i] = this.tablaNotas[i, numAlumno - 1];
+            }
+
+            return notasAlumno;
+        }
+
+        public int[] verNotasAsignatura(int numAsignatura)
+        {
+            int[] notasAsignatura = new int[this.tablaNotas.GetLength(1)];
+            for (int i = 0; i < this.tablaNotas.GetLength(1); i++)
+            {
+                notasAsignatura[i] = this.tablaNotas[numAsignatura - 1, i];
+            }
+            return notasAsignatura;
+        }
+
+        public int[] notaMaxMinAlumno(ref int numAlumno)
+        {
+            int[] notasAlumno = new int[4];
+            int menor = 10;
+            int mayor = 0;
+
+            for (int i = 0; i < this.tablaNotas.GetLength(0); i++)
+            {
+                notasAlumno[i] = this.tablaNotas[i, numAlumno - 1];
+            }
+
+            for (int i = 0; i < notasAlumno.Length; i++)
+            {
+                if (notasAlumno[i] < menor)
+                {
+                    menor = notasAlumno[i];
+                }
+                if (notasAlumno[i] > mayor)
+                {
+                    mayor = notasAlumno[i];
+                }
+            }
+            int[] notas = { menor, mayor };
+            return notas;
+        }
+
+        public int[,] tAprobados()
+        {
+            int asigAprobadas = 0;
+            int numColumnas = 0;
+            List<int> indices = new List<int>();
+            int[,] soloAprobados = null;
+            for (int i = 0; i < tablaNotas.GetLength(1); i++)
+            {
+                asigAprobadas = 0;
+                for (int j = 0; j < tablaNotas.GetLength(0); j++)
+                {
+                    if (tablaNotas[j, i] >= 5)
+                    {
+                        asigAprobadas++;
+
+                    }
+                }
+
+                if (asigAprobadas >= tablaNotas.GetLength(0))
+                {
+                    numColumnas++;
+                    indices.Add(i);
+                }
+
+            }
+
+            soloAprobados = new int[tablaNotas.GetLength(0), numColumnas];
+
+            for (int i = 0; i < soloAprobados.GetLength(0); i++)
+            {
+                for (int j = 0; j < soloAprobados.GetLength(1); j++)
+                {
+
+                    soloAprobados[i, j] = tablaNotas[i, indices[j]];
+                }
+            }
+            return soloAprobados;
+        }
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aula aula = new Aula();
+            aula.rellenarTabla();
+            int opcion = 0;
+            int alumno;
+            int asignatura;
+            do
+            {
+                Console.WriteLine("Selecciona una opcion:");
+                Console.WriteLine("1. Calcular la media de notas de toda la tabla");
+                Console.WriteLine("2. Media de un alumno");
+                Console.WriteLine("3. Media de una asignatura");
+                Console.WriteLine("4. Visualizar notas de un alumno");
+                Console.WriteLine("5. Visualizar notas de una asignatura");
+                Console.WriteLine("6. Nota máxima y mínima de un alumno");
+                Console.WriteLine("7. Tabla solo de aprobados");
+                Console.WriteLine("8. Visualizar tabla completa");
+                Console.WriteLine("9. Salir del programa");
+                opcion = pedirInt();
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("La media de todas las notas es: {0:N2}", aula.calcularMediaTotal());
+                        Console.WriteLine();
+                        Console.WriteLine("Pulsa ENTER para continuar");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        do
+                        {
+                            Console.WriteLine("Selecciona un alumno");
+                            alumno = pedirInt();
+                        } while (alumno <= 0 || alumno > 12);
+                        Console.WriteLine("La media de las notas de {0} es : {1:N2}", aula[alumno - 1], aula.calcularMediaAlumno(alumno));
+                        Console.WriteLine();
+                        Console.WriteLine("Pulsa ENTER para continuar");
+                        Console.ReadLine();
+                        break;
+
+                    case 3:
+                        do
+                        {
+                            Console.WriteLine("Selecciona una asignatura");
+                            asignatura = pedirInt();
+                        } while (asignatura <= 0 || asignatura > 4);
+                        Console.WriteLine("La media de las notas es: {0:N2}", aula.calcularMediaAsignatura(asignatura));
+                        Console.WriteLine();
+                        Console.WriteLine("Pulsa ENTER para continuar");
+                        Console.ReadLine();
+                        break;
+
+                    case 4:
+                        do
+                        {
+                            Console.WriteLine("Selecciona un alumno");
+                            alumno = pedirInt();
+                        } while (alumno <= 0 || alumno>12);
+                        int[] notasAlumno = aula.verNotasAlumno(alumno);
+                        Console.WriteLine("Las notas de {0} son: ", aula[alumno - 1]);
+                        for(int i=0; i < notasAlumno.Length; i++)
+                        {
+                            Console.WriteLine("{0}\t", notasAlumno[i]);
+                        }
+                        Console.Write("\n");
+                        Console.WriteLine();
+                        Console.WriteLine("Pulsa ENTER para continuar");
+                        Console.ReadLine();
+                        break;
+
+                    case 5:
+                        do
+                        {
+                            Console.WriteLine("Selecciona una asignatura");
+                            asignatura = pedirInt();
+
+                        } while (asignatura <= 0 || asignatura>4);
+                        int[] notasAsignatura = aula.verNotasAsignatura(asignatura);
+                        Console.WriteLine("Las notas son: ");
+                        for(int i=0; i < notasAsignatura.Length; i++)
+                        {
+                            Console.WriteLine("{0} :\t", aula[i]);
+                            Console.WriteLine("{0}\n", notasAsignatura[i]);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Pulsa ENTER para continuar");
+                        Console.ReadLine();
+                        break;
+
+                    case 6:
+                        do
+                        {
+                            Console.WriteLine("Selecciona un alumno");
+                            alumno = pedirInt();
+                        } while (alumno <= 0 || alumno>12);
+                        int[] notaMaxMin = aula.notaMaxMinAlumno(ref alumno);
+                        Console.WriteLine("La nota más baja de {0} es: {1}", aula[alumno-1], notaMaxMin[0]);
+                        Console.WriteLine("La nota más alta de {0} es: {1}", aula[alumno-1], notaMaxMin[1]);
+                        Console.WriteLine();
+                        Console.WriteLine("Pulsa ENTER para continuar");
+                        Console.ReadLine();
+                        break;
+
+                    case 7:
+                        for (int i=0; i<aula.tAprobados().GetLength(0); i++)
+                        {
+                            for (int j=0; j<aula.tAprobados().GetLength(1);j++)
+                            {
+                                Console.Write("{0,3}",aula.tAprobados()[i, j]); 
+                            }
+                            Console.WriteLine();
+                        }
+
+                        //Console.WriteLine(aula.tAprobados().GetLength(1));
+                        break;
+
+                    case 8:
+                        for(int i=0; i<12; i++)
+                        {
+                            if (i == 0)
+                            {
+                                Console.Write(String.Format("{0,20}", aula[i]));
+                            }else if (i == 11)
+                            {
+                                Console.Write(String.Format("{0,12}", aula[i]));
+                            }
+                            else
+                            {
+                                Console.Write(String.Format("{0,8}", aula[i]));
+                            }
+                        }
+                        Console.WriteLine();
+
+                        Aula.asignaturas asig;
+                        for (int i=0; i < aula.longitudTabla(0); i++)
+                        {
+                            asig = (Aula.asignaturas)i;
+                            Console.Write(String.Format("{0, -12}", asig));
+
+                            for(int j=0; j < aula.longitudTabla(1); j++)
+                            {
+                                if (j == 11)
+                                {
+                                    Console.Write(String.Format("{0,10}",aula[i,j]));
+                                }
+                                else
+                                {
+                                    Console.Write(String.Format("{0,8}",aula[i,j]));
+                                }
+                            }
+                            Console.WriteLine("\n");
+                        }
+                        break;
+
+                    case 9:
+                        Console.WriteLine("Saliendo del programa");
+                        break;
+
+                    default:
+                        Console.WriteLine("Error, opción no válida");
+                        break;
+                }
+            } while (opcion != 9);
+        }
+
+        public static int pedirInt()
+        {
+            int num = 0;
+            do
+            {
+                try
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                    return num;
+                }
+                catch (System.OverflowException)
+                {
+                    Console.WriteLine("Error, valor no válido, introduce otro");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Error, valor no válido, introduce otro");
+                }
+            } while (num == 0);
+            return 0;
+        }
+    }
+}
